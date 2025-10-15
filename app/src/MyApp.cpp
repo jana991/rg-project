697b29d6251ae998e04c9daba5df6db06d9cfbaa
matfrg <janaradivojevic999@gmail.com>
2025-10-15 12:25:46 +0200

    Added basic app setup

---
+++ app/src/MyApp.cpp
@@ -0,0 +1,14 @@
+//
+// Created by matfrg on 10/15/25.
+//
+
+#include "../include/MyApp.h"
+
+#include "../../engine/libs/spdlog/include/spdlog/spdlog.h"
+
+namespace app {
+
+void MyApp::app_setup() {
+    spdlog::info("App setup completed");
+}
+} // app
