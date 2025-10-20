f13b69f8027d2e9c019be3df3439dc62e6022e2c
matfrg <janaradivojevic999@gmail.com>
2025-10-20 18:02:12 +0200

    Changed the model for the lighthouse, draw lighthouse

--- app/include/MainController.h
+++ app/include/MainController.h
@@ -10,6 +10,14 @@ namespace app {
 class MainController: public engine::core::Controller {
     void initialize() override;
     bool loop() override;
+    void draw_lighthouse();
+
+    void begin_draw() override;
+    void draw() override;
+
+    void end_draw() override;
+
+
 
 public:
     std::string_view name() const override {
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,13 +4,17 @@
 
 #include "MainController.h"
 
+#include "../../engine/libs/assimp/code/AssetLib/3MF/3MFXmlTags.h"
 #include "../../engine/libs/spdlog/include/spdlog/spdlog.h"
 
+#include <engine/graphics/GraphicsController.hpp>
+#include <engine/graphics/OpenGL.hpp>
 #include <engine/platform/PlatformController.hpp>
+#include <engine/resources/ResourcesController.hpp>
 
 namespace app {
 void MainController::initialize() {
-    spdlog::info('Controller initialized');
+    engine::graphics::OpenGL::enable_depth_testing();
 }
 bool MainController::loop() {
     auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
@@ -18,6 +22,34 @@ bool MainController::loop() {
         return false;
     }
     return true;
+}
+void MainController::draw_lighthouse() {
+    //model
+    auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+    auto graphics=engine::core::Controller::get<engine::graphics::GraphicsController>();
+    engine::resources::Model *lighthouse = resources->model("lighthouse");
+    //shader
+    engine::resources::Shader *shader = resources->shader("lighthouse");
+    shader->use();
+    shader->set_mat4("projection",graphics->projection_matrix());
+    shader->set_mat4("view",graphics->camera()->view_matrix());
+    glm::mat4 model=glm::mat4(1.0f);
+    model=glm::translate(model,glm::vec3(0.0f,-1.0f,-3.0f));
+    model=glm::scale(model,glm::vec3(0.3f));
+    shader->set_mat4("model",model);
+    lighthouse->draw(shader);
+}
+void MainController::begin_draw() {
+    engine::graphics::OpenGL::clear_buffers();
+}
+void MainController::draw() {
+    //clear buffers (color buffer, depth buffer)
+    draw_lighthouse();
+    //swap buffer
+}
+void MainController::end_draw() {
+    auto platform=engine::core::Controller::get<engine::platform::PlatformController>();
+    platform->swap_buffers();
 }
 
 
