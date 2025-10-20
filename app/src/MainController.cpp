d00461d03220a571d4119c6a3119fb3e774d75bc
matfrg <janaradivojevic999@gmail.com>
2025-10-20 20:03:57 +0200

    Added skybox and draw skybox

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -13,18 +13,18 @@
 #include <engine/resources/ResourcesController.hpp>
 
 namespace app {
-class MainPlatformEventObserver:public engine::platform::PlatformEventObserver {
+class MainPlatformEventObserver : public engine::platform::PlatformEventObserver {
 public:
     void on_mouse_move(engine::platform::MousePosition position) override;
 };
 void MainPlatformEventObserver::on_mouse_move(engine::platform::MousePosition position) {
-    auto camera=engine::core::Controller::get<engine::graphics::GraphicsController>()->camera();
-    camera->rotate_camera(position.dx,position.dy);
+    auto camera = engine::core::Controller::get<engine::graphics::GraphicsController>()->camera();
+    camera->rotate_camera(position.dx, position.dy);
 }
 
 
 void MainController::initialize() {
-    auto platform=engine::core::Controller::get<engine::platform::PlatformController>();
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
     platform->register_platform_event_observer(std::make_unique<MainPlatformEventObserver>());
     engine::graphics::OpenGL::enable_depth_testing();
 }
@@ -52,21 +52,21 @@ void MainController::draw_lighthouse() {
     lighthouse->draw(shader);
 }
 void MainController::update_camera() {
-    auto platform=engine::core::Controller::get<engine::platform::PlatformController>();
-    auto graphics=engine::core::Controller::get<engine::graphics::GraphicsController>();
-    auto camera=graphics->camera();
-    float dt=platform->dt();
-    if(platform->key(engine::platform::KeyId::KEY_W).is_down()) {
-        camera->move_camera(engine::graphics::Camera::Movement::FORWARD,dt);
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    auto camera = graphics->camera();
+    float dt = platform->dt();
+    if (platform->key(engine::platform::KeyId::KEY_W).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::FORWARD, dt);
     }
-    if(platform->key(engine::platform::KeyId::KEY_A).is_down()) {
-        camera->move_camera(engine::graphics::Camera::Movement::LEFT,dt);
+    if (platform->key(engine::platform::KeyId::KEY_A).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::LEFT, dt);
     }
-    if(platform->key(engine::platform::KeyId::KEY_S).is_down()) {
-        camera->move_camera(engine::graphics::Camera::Movement::BACKWARD,dt);
+    if (platform->key(engine::platform::KeyId::KEY_S).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::BACKWARD, dt);
     }
-    if(platform->key(engine::platform::KeyId::KEY_D).is_down()) {
-        camera->move_camera(engine::graphics::Camera::Movement::RIGHT,dt);
+    if (platform->key(engine::platform::KeyId::KEY_D).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::RIGHT, dt);
     }
 }
 void MainController::update() {
@@ -75,13 +75,21 @@ void MainController::update() {
 void MainController::begin_draw() {
     engine::graphics::OpenGL::clear_buffers();
 }
+void MainController::draw_skybox() {
+    auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+    auto skybox = resources->skybox("skyboxbak");
+    auto shader = resources->shader("skybox");
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    graphics->draw_skybox(shader, skybox);
+}
 void MainController::draw() {
     //clear buffers (color buffer, depth buffer)
     draw_lighthouse();
+    draw_skybox();
     //swap buffer
 }
 void MainController::end_draw() {
-    auto platform=engine::core::Controller::get<engine::platform::PlatformController>();
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
     platform->swap_buffers();
 }
 
