57ec1ce87c8595cd3037d84e2df3427c0a8b6800
matfrg <janaradivojevic999@gmail.com>
2025-10-26 22:15:36 +0100

    Added option to reset values in GUI

--- app/src/GUIController.cpp
+++ app/src/GUIController.cpp
@@ -42,8 +42,27 @@ void GUIController::draw() {
     ImGui::SliderFloat("Dir Ambient", &main->dirLightAmbientIntensity, 0.0f, 1.0f, "%.3f");
     ImGui::SliderFloat("Dir Diffuse", &main->dirLightDiffuseIntensity, 0.0f, 2.0f, "%.3f");
     ImGui::SliderFloat("Dir Specular", &main->dirLightSpecularIntensity, 0.0f, 2.0f, "%.3f");
-    ImGui::End();
     ImGui::Separator();
+    if (ImGui::Button("Reset Spotlight Params")) {
+        main->spotlightColor = glm::vec3(1.0f);
+        main->spotlightAmbientIntensity = 0.1f;
+        main->spotlightDiffuseIntensity = 4.0f;
+        main->spotlightSpecularIntensity = 2.0f;
+        main->spotlightCutOffDeg = 6.0f;
+        main->spotlightOuterCutOffDeg = 10.0f;
+        main->spotlightConstant = 1.0f;
+        main->spotlightLinear = 0.09f;
+        main->spotlightQuadratic = 0.032f;
+    }
+
+
+    if (ImGui::Button("Reset DirLight")) {
+        main->dirLightColor = glm::vec3(1.0f);
+        main->dirLightAmbientIntensity = 0.05f;
+        main->dirLightDiffuseIntensity = 0.4f;
+        main->dirLightSpecularIntensity = 0.5f;
+    }
+    ImGui::End();
 
     graphics->end_gui();
 }
